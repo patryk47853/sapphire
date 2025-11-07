47be51ddeb386fec072cfaccbada00f404de04dd
Patryk <109095669+patryk47853@users.noreply.github.com>
2025-11-07 13:03:18 +0100

    Initial commit

---
+++ src/test/java/com/patrykjava/sapphire/SapphireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrykjava.sapphire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapphireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

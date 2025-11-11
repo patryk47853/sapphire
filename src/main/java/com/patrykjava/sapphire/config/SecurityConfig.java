55e9af2a24b17fbfe3033f299559548a70234ad2
Patryk <109095669+patryk47853@users.noreply.github.com>
2025-11-11 13:48:45 +0100

    Adjust current SecurityConfig for test endpoints

--- src/main/java/com/patrykjava/sapphire/config/SecurityConfig.java
+++ src/main/java/com/patrykjava/sapphire/config/SecurityConfig.java
@@ -3,26 +3,24 @@ package com.patrykjava.sapphire.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-      .csrf(AbstractHttpConfigurer::disable)
-      .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
-        .anyRequest().authenticated()
-      )
-      .oauth2ResourceServer(oauth2 -> oauth2
-        .jwt(withDefaults())
-      );
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/test/admin").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/api/test/user").hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
 }

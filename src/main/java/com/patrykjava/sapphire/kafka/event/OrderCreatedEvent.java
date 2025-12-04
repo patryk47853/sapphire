a7d1fd8eca82abd8a239d302695c47f463f4d303
Patryk <109095669+patryk47853@users.noreply.github.com>
2025-12-04 12:58:35 +0100

    Implement OrderCreatedEvent

---
+++ src/main/java/com/patrykjava/sapphire/kafka/event/OrderCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.patrykjava.sapphire.kafka.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderCreatedEvent {
+    private Long orderId;
+    private Long userId;
+    private Double totalPrice;
+}
